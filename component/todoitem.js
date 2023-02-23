ddc2b614f149e69f1858dc0c6ba3fa61eb7dcd4a
NGUYEN MINH TUAN <ltvminhtuan99@gmail.com>
2023-02-23 17:04:44 +0700

    todolist

--- component/todoitem.js
+++ component/todoitem.js
@@ -1,16 +1,29 @@
 import html from '../core.js';
+import { connect } from '../store.js';
 
-function todoItem({ todo }) {
+function todoItem({ todo, index, editIndex }) {
     return html`
-        <li class="${todo.completed && 'completed'}">
+        <li 
+            class="${todo.completed && 'completed'}
+            ${editIndex === index && 'editing'}"
+        >
             <div class="view">
-                <input class="toggle" type="checkbox" ${todo.completed && 'checked'}>
-                <label>${todo.title}</label>
-                <button class="destroy"></button>
+                <input class="toggle" 
+                type="checkbox" 
+                ${todo.completed && 'checked'}
+                onchange="dispatch('toggle', ${index})"
+                >
+                <label ondblclick="dispatch('startEdit', ${index})">${todo.title}</label>
+                <button class="destroy" onclick="dispatch('destroy', ${index})"></button>
             </div>
-            <input class="edit" value="${todo.title}">
+            <input 
+                class="edit" 
+                value="${todo.title}"
+                onkeyup="event.keyCode === 13 && dispatch('endEdit', this.value.trim()) || event.keyCode === 27 && dispatch('cancelEdit', this.value.trim()) "
+                onblur="dispatch('endEdit', this.value.trim())"
+            >
         </li>
         `
 };
 
-export default todoItem;
+export default connect()(todoItem);
--- component/todolist.js
+++ component/todolist.js
@@ -2,13 +2,22 @@ import html from '../core.js';
 import todoItem from './todoitem.js';
 import { connect } from '../store.js';
 
-function todolist({ todos }) {
+function todolist({ todos, filter, filters }) {
     return html`
             <section class="main">
-				<input id="toggle-all" class="toggle-all" type="checkbox">
+				<input id="toggle-all" 
+				class="toggle-all" 
+				type="checkbox"
+				onchange="dispatch('toggleAll', this.checked)"
+				${todos.every(filters.completed) && 'checked="checked"'}
+				>
 				<label for="toggle-all">Mark all as complete</label>
 				<ul class="todo-list">
-                    ${todos.map(todo => todoItem({todo}))}
+                    ${todos
+						.filter(filters[filter])
+						.map((todo, index) => 
+						todoItem({todo, index})
+						)}
 				</ul>
 			</section>
             `
